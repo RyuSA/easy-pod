535e55f0048844d7cfd645ec65ea2132fc70ecd5
RyuSA <kommsussertad@gmail.com>
2020-08-12 02:21:50 +0000

    add accesslog

---
+++ app/src/main/java/com/example/demo/configuration/TomcatConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+
+@Configuration
+public class TomcatConfiguration {
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addContextValves(new LogbackValve());
+        return tomcatServletWebServerFactory;
+    }    
+}
