2af630275db0ba543a40a0b8db989b68f936654e
R.S.A <kommsussertad@gmail.com>
2020-08-19 13:36:30 +0900

    Add @RestController and @RequestMapping

--- app/src/main/java/com/example/demo/controller/PodInformationController.java
+++ app/src/main/java/com/example/demo/controller/PodInformationController.java
@@ -3,7 +3,11 @@ package com.example.demo.controller;
 import java.net.InetAddress;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/pod")
 public class PodInformationController {
     @GetMapping(path = "/os")
     public String os() {
